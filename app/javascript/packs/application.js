3b80a1e0b3eefd67c4794cd72a893e8632c4c8b5
saotome88 <ryoji.saotome1988@gmail.com>
2021-08-29 21:36:14 +0900

    application.js/import fontawesomeを記述

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 Turbolinks.start()
@@ -10,6 +11,7 @@ ActiveStorage.start()
 // 作成したファイルをwebpackerに読み込ませます。
 import "../stylesheets/application"
 import "../js/hoge.js"
+
 
 // - 検証用 -
 // app/javascript/images/フォルダー内の001.jpeg画像を
