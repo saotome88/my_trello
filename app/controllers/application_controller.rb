96e203ef045c747329e417b5ff4834f31d37a7fc
saotome88 <ryoji.saotome1988@gmail.com>
2021-09-02 19:12:11 +0900

    nicknameカラム用にstrongパラメーターの記述を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  #クロスサイトリクエストフォージェリ (CSRF)への対応策のコード
+  protect_from_forgery with: :exception
+
   before_action :authenticate_user!
+  #deviseコントローラーを使用するときにのみ処理をする
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  endend
+  end
 end
