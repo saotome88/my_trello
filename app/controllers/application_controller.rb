c154ba110a5e0f4666fddc5c80156a2b9979a215
saotome88 <ryoji.saotome1988@gmail.com>
2021-09-02 19:19:55 +0900

    application_controllerにあるendの重複を修正した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
-  endend
   end
 end
