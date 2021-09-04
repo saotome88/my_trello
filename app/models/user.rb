842afdb1627081170359b63e9c3631b868e48b8d
saotome88 <ryoji.saotome1988@gmail.com>
2021-09-04 23:28:02 +0900

    バリデーションエラーの解消/nameをnicknameに修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name,presence: true, length: {maximum: 20}
+  validates :nickname,presence: true, length: {maximum: 20}
 end
