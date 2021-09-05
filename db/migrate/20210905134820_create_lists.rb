8431d55a2e1659ab71754a14546572ac80131bdc
saotome88 <ryoji.saotome1988@gmail.com>
2021-09-05 22:52:09 +0900

    listsテーブルのmigrationファイルを編集

--- db/migrate/20210905134820_create_lists.rb
+++ db/migrate/20210905134820_create_lists.rb
@@ -3,6 +3,8 @@ class CreateLists < ActiveRecord::Migration[6.0]
     create_table :lists do |t|
 
       t.timestamps
+      t.string     :title, null: false
+      t.references :user,  foreign_key: true, null: false
     end
   end
 end
