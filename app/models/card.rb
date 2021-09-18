40400f0f86554ef4fa26f4b8b1e2b382bdb46bd6
saotome88 <ryoji.saotome1988@gmail.com>
2021-09-18 23:32:31 +0900

    cardモデルのバリデーションの記述修正

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :list
-
+  
   validates :title, length: {in: 1..255}
-  validates :memo, length: {in: maximum: 1000}
+  validates :memo, length: {maximum: 1000}
 end
